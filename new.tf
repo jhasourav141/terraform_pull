8f1c4c50bdf3af23c7796736a7d35785f3e4ed3c
Sourav Kumar Jha <60103363+jhasourav141@users.noreply.github.com>
2022-07-04 13:00:59 +0530

    Update and rename README.md to new.tf

---
+++ new.tf
@@ -0,0 +1,33 @@
+provider "aws" {
+  region = "us-east-1"
+   access_key = "AKIAZRJDC5HD2MMBF47V"
+  secret_key = "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"
+  }
+# 1. Create VPC
+
+resource "aws_vpc" "second_vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "prod "
+  }
+}
+#2.create Subnet 
+
+resource "aws_subnet" "my_subnet_2" {
+  vpc_id            = aws_vpc.second_vpc.id
+  cidr_block        = "10.0.0.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "prod-sn"
+  }
+}
+#3. Create ICW 
+
+resource "aws_internet_gateway" "my-IGW1" {
+  vpc_id = aws_vpc.second_vpc.id
+
+}
+
+
